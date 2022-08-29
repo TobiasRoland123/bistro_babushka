3f339a04c18281ed055f6c2a5f1a6ed3c81e5779
Tobias Roland <samu1493@stud.kea.dk>
2022-08-29 13:09:01 +0200

    first commit

---
+++ script.js
@@ -0,0 +1,53 @@
+const url = "https://babushka-dd8a.restdb.io/rest/menu";
+const template = document.querySelector("template").content;
+const main = document.querySelector("#indhold");
+const header = document.querySelector("h1");
+
+const option = {
+  headers: {
+    "x-apikey": "600ec2fb1346a1524ff12de4",
+  },
+};
+
+document.addEventListener("DOMContentLoaded", start);
+let retter;
+let filter = "alle";
+
+function start() {
+  const filterKnapper = document.querySelectorAll("nav button");
+  filterKnapper.forEach((knap) => knap.addEventListener("click", filtrerRetter));
+  hentData();
+}
+
+function filtrerRetter() {
+  filter = this.dataset.kategori;
+  // console.log(this.dataset.kategori);
+  document.querySelector(".valgt").classList.remove("valgt");
+  this.classList.add("valgt");
+  header.textContent = this.textContent;
+  vis();
+}
+
+async function hentData() {
+  const respons = await fetch(url, option);
+  retter = await respons.json();
+  // console.log(retter);
+  vis();
+}
+
+function vis() {
+  main.textContent = "";
+  retter.forEach((ret) => {
+    if (filter == ret.kategori || filter == "alle") {
+      const klon = template.cloneNode(true);
+      // klon.querySelector("img").src = ret.billede;
+      klon.querySelector(".navn").textContent = ret.navn;
+      klon.querySelector(".info").textContent = ret.kategori + " | " + ret.oprindelsesregion;
+      klon.querySelector(".kortbeskrivelse").textContent = ret.kortbeskrivelse;
+      klon.querySelector(".pris").textContent = "Pris: " + ret.pris + ",-";
+      main.appendChild(klon);
+    }
+  });
+}
+
+hentData();
