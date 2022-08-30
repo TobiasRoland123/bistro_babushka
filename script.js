de2c60be8602172b1136a4eece46c69b94b4c7c2
Tobias Roland <samu1493@stud.kea.dk>
2022-08-30 14:12:05 +0200

    babuska med separat singeview

--- script.js
+++ script.js
@@ -2,7 +2,7 @@ const url = "https://babushka-dd8a.restdb.io/rest/menu";
 const template = document.querySelector("template").content;
 const main = document.querySelector("#indhold");
 const header = document.querySelector("h1");
-
+const popup = document.querySelector("#popup");
 const option = {
   headers: {
     "x-apikey": "600ec2fb1346a1524ff12de4",
@@ -24,14 +24,14 @@ function filtrerRetter() {
   // console.log(this.dataset.kategori);
   document.querySelector(".valgt").classList.remove("valgt");
   this.classList.add("valgt");
-  header.textContent = this.textContent;
+
   vis();
 }
 
 async function hentData() {
   const respons = await fetch(url, option);
   retter = await respons.json();
-  // console.log(retter);
+  console.log(retter);
   vis();
 }
 
@@ -41,14 +41,32 @@ function vis() {
     if (filter == ret.kategori || filter == "alle") {
       const klon = template.cloneNode(true);
       klon.querySelector("img").src = "billeder/" + ret.billednavn + "-md.jpg";
-      console.log(ret.billednavn);
+      // console.log(ret.billednavn);
       klon.querySelector(".navn").textContent = ret.navn;
       klon.querySelector(".info").textContent = ret.kategori + " | " + ret.oprindelsesregion;
       klon.querySelector(".kortbeskrivelse").textContent = ret.kortbeskrivelse;
       klon.querySelector(".pris").textContent = "Pris: " + ret.pris + ",-";
+
+      klon.querySelector("article").addEventListener("click", () => visDetaljer(ret));
+
       main.appendChild(klon);
     }
   });
+}
+
+function visDetaljer(retten) {
+  console.log(retten.navn);
+
+  location.href = "02-detalje.html?id=" + retten._id;
+
+  // popup.style.display = "flex";
+  // popup.querySelector("img").src = "billeder/" + retten.billednavn + "-md.jpg";
+  // popup.querySelector(".titel").textContent = retten.navn;
+  // popup.querySelector(".korttekst").textContent = retten.langbeskrivelse;
+  // popup.querySelector(".typeogland").textContent = retten.kategori + " | " + retten.oprindelsesregion;
+  // popup.querySelector(".pris").textContent = "Pris: " + retten.pris + ",-";
+
+  // document.querySelector("#luk").addEventListener("click", () => (popup.style.display = "none"));
 }
 
 hentData();
